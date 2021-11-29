f5f7b13ed99062b51ab2ff6b151864ede83b4ac2
Windless95 <barrerajoelj@gmail.com>
2021-11-29 23:45:12 +0000

    need to fix button link script

--- src/app.js
+++ src/app.js
@@ -6,19 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let a = ["the", "our", "random"];
-  let b = ["pretty", "amazing", "sweet"];
-  let c = ["racoon", "trashpanda", "redpanda"];
-  let d = [".com", ".net", ".org"];
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-  for (let z of a) {
-    for (let y of b) {
-      for (let x of c) {
-        for (let w of d) {
-          document.querySelector("#Wizardbirb").innerHTML +=
-            "<li>" + z + y + x + w + "<li>";
-        }
-      }
-    }
+  let who1 = who[Math.floor(Math.random() * 4)];
+  let action1 = action[Math.floor(Math.random() * 4)];
+  let what1 = what[Math.floor(Math.random() * 4)];
+  let when1 = when[Math.floor(Math.random() * 4)];
+
+  document.querySelector("#excuse").innerHTML =
+    who1 + " " + action1 + " " + what1 + " " + when1;
+
+  function addingWho() {
+    boxvalue = document.getElementById("box").value;
+    who.push(boxvalue);
+    console.log(who);
+    return false;
   }
 };
